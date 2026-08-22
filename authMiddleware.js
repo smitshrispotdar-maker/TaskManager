d8fde9077706902d5b0d4f0b8d7b97f81d3b3244
Smitshri Potdar <smitshrispotdar@gmail.com>
2026-08-22 23:34:30 +0530

    Update authMiddleware.js

--- authMiddleware.js
+++ authMiddleware.js
@@ -5,18 +5,18 @@ const protect = async (req, res, next) => {
   try {
     let token;
 
-    // Check if Authorization header exists
+    
     if (
       req.headers.authorization &&
       req.headers.authorization.startsWith("Bearer")
     ) {
-      // Extract token
+      
       token = req.headers.authorization.split(" ")[1];
 
-      // Verify token
+      
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      // Find user (excluding password)
+      
       req.user = await User.findById(decoded.id).select("-password");
 
       next();
